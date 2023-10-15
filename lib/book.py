61fcc0a5dbf316211fad0ccfa51cdbbe904fdf66
Olayanju Oluwadamilare <hadreytechsolutions@gmail.com>
2023-10-15 20:16:03 +0100

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,name,types):
+        self.name = name 
+        self.types = types 
+        
+    def get_name(self):
+        print(f"This is {self.name} book")
+        return self.name
+    
+    def get_type(self):
+        print(f"This is {self.types} type of book")
+        return self.types
+    
+
+s1 = Book("Biology","2D")
+s1.get_name()
+s1.get_type()
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,name,model,type):
+        self.name = name 
+        self.model = model
+        self.type = type
+        
+    def get_name(self):
+        print(f"This is a {self.name} shoe")
+        
+    def get_model(self):
+        print(f"The model of this shoe is {self.model}")
+        
+    def get_type(self):
+        print(f"This is a {self.type} type of shoe")
+
+s2 = Shoe("Bola","Joshua","Kunle")
+print(s2.get_model())
+print(s2.get_name())
+print(s2.get_type())
